15e3bf50fa400ca7e9e38098cf1ad250d4047f58
Asedem <asedem.games@gmail.com>
2023-08-25 23:04:05 +0200

    Initial commit

---
+++ src/main/java/de/asedem/fileexplorer/FileExplorer.java
@@ -0,0 +1,17 @@
+package de.asedem.fileexplorer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FileExplorer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
